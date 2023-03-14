0f018a8989213dac8617db6e97fc92fdff81043c
Rachelle Cansier <chelxie98@hotmail.com>
2023-03-14 10:24:26 +0100

    changement

--- tableauobjets.js
+++ tableauobjets.js
@@ -1,5 +1,7 @@
 const personnes = [
-    {nom: 'Rachelle' ;age: '24' ; emploi: 'chef de projet' }
-    {nom: 'Pierre' ;age: '25' ; emploi: 'Artiste' }
-    {nom: 'Cyril' ;age: '26' ; emploi: 'controleur' }
-    {nom: 'Marie' ;age: '23' ; emploi: 'agent immobilier' }
+    {nom: 'Rachelle' ,age: 24 , emploi: 'chef de projet' };
+    {nom: 'Pierre' ,age: 25 , emploi: 'Artiste' };
+    {nom: 'Cyril' ,age: 26 , emploi: 'controleur' };
+    {nom: 'Marie' ,age: 23 , emploi: 'agent immobilier' };
+]
+
